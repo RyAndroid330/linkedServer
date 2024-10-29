23a92ce59227f1c3a63a88a42096208d8fd1ebeb
RyAndroid <ryan.w.simpson@gmail.com>
2024-10-29 17:00:12 +0100

    3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+export default {
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
+};
